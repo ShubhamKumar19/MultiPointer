a0c6b59b6bd293bc219125c4b4041db87f0deba4
Shubham <shubhamkumars19061998@gmail.com>
2021-08-07 16:12:38 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,23 @@
+//We can implement multiple pointers if array is sorted
+function UniqueValues(arr)
+{
+    arr=arr.sort();
+   if(!arr.length)
+      return 0;
+   let i=0;
+   let j=i+1;
+   let count=0;
+    while(j<=arr.length) {
+        if(arr[i]===arr[j]) {
+            j+=1;
+         
+        }
+        else {
+            count++;
+            i=j;
+            j++;    
+        }
+    }
+    return count++;
+}
+console.log(UniqueValues([1,4,2,2,5,7,7,9,9]))
